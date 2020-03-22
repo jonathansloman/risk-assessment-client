214f4a77e1ebb5f7e5770dfcfe8aecf15193c067
Jonathan Sloman <jonathan.sloman@bbc.co.uk>
2020-03-22 00:38:09 +0000

    Make it connect!

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,9 @@
 
 const AppConfig = {
-    PROTOCOL: "wss:",
+    PROTOCOL: "ws:",
     // TODO: change to localhost if you wish to run it locally
     HOST: "//localhost",
-    PORT: ":9002"
+    PORT: ":3001"
 }
 
 const Singleton = (function () {
@@ -11,7 +11,7 @@ const Singleton = (function () {
 
     function createInstance() {
         // TODO: add +  PORT if you want to run it locally
-        const socket = new WebSocket(AppConfig.PROTOCOL + AppConfig.HOST);
+        const socket = new WebSocket(AppConfig.PROTOCOL + AppConfig.HOST + AppConfig.PORT);
         return socket;
     }
 
